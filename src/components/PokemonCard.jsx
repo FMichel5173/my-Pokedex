d3e0f733c174d02ee7facce2b85fae3c04c0dafb
FMichel5173 <fmichel81@sfr.fr>
2023-08-14 15:23:38 +0200

    create components.2 branch and modication of PokemonCard to  dynamic display

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,40 +1,28 @@
+const pokemonList = [
+  {
+    name: "Bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "Charmander",
+  },
+];
+
 const PokemonCard = () => {
+  const pokemon = pokemonList[0]; // Changez ceci à pokemonList[1] pour tester le second Pokémon.
+
   return (
-    <div className="cards">
-      <figure className="card-bulbasaur">
-        <img
-          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
-          alt='Bulbasaur'
-          className="card-img"
-        />
-        <figcaption>Bulbasaur</figcaption>
-      </figure>
-      <figure className="card-charmander">
-        <img
-          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
-          alt='Charmander'
-          className="card-img"
-        />
-        <figcaption>Charmander</figcaption>
-      </figure>
-      <figure className="card-pikachu">
-        <img
-          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png'
-          alt='Pikachu'
-          className="card-img"
-        />
-        <figcaption>Pikachu</figcaption>
-      </figure>
-      <figure className="card-squirtle">
-        <img
-          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png'
-          alt='Squirtle'
-          className="card-img"
-        />
-        <figcaption>Squirtle</figcaption>
+    <div className='cards'>
+      <figure className='card'>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} className='card-img' />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>{pokemon.name}</figcaption>
       </figure>
     </div>
-    
   );
 };
 
