8f43b30f916c4f629f85ec6ed9fd6ed3b79b80ab
FMichel5173 <fmichel81@sfr.fr>
2023-08-14 11:52:27 +0200

    Create multiple Pokemon and applying the style

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,14 +1,40 @@
 const PokemonCard = () => {
   return (
-    <div>
-      <figure>
+    <div className="cards">
+      <figure className="card-bulbasaur">
         <img
           src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png'
           alt='Bulbasaur'
+          className="card-img"
         />
         <figcaption>Bulbasaur</figcaption>
       </figure>
+      <figure className="card-charmander">
+        <img
+          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png'
+          alt='Charmander'
+          className="card-img"
+        />
+        <figcaption>Charmander</figcaption>
+      </figure>
+      <figure className="card-pikachu">
+        <img
+          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png'
+          alt='Pikachu'
+          className="card-img"
+        />
+        <figcaption>Pikachu</figcaption>
+      </figure>
+      <figure className="card-squirtle">
+        <img
+          src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png'
+          alt='Squirtle'
+          className="card-img"
+        />
+        <figcaption>Squirtle</figcaption>
+      </figure>
     </div>
+    
   );
 };
 
