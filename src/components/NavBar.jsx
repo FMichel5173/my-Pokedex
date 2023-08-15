688da011f3265d277235eddad0b027c8400e2c61
FMichel5173 <fmichel81@sfr.fr>
2023-08-15 18:34:32 +0200

    Moved handles function and fix PropTypes in NavBAr

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,19 +1,35 @@
 import PropTypes from "prop-types";
 
-const NavBar = ({pokemonList, pokemonIndex,hanhdleClickIncr, hanhdleClickDecr}) => {
+const NavBar = ({ pokemonList, pokemonIndex, setPokemonIndex }) => {
+  const hanhdleClickIncr = () => {
+    setPokemonIndex(pokemonIndex + 1);
+  };
+
+  const hanhdleClickDecr = () => {
+    setPokemonIndex(pokemonIndex - 1);
+  };
+
   return (
     <div>
-      {pokemonIndex > 0 && <button onClick={hanhdleClickDecr}>Précédent</button>}
-      {pokemonIndex < pokemonList.length - 1 && <button onClick={hanhdleClickIncr}>Suivant</button>}
+      {pokemonIndex > 0 && (
+        <button onClick={hanhdleClickDecr}>Précédent</button>
+      )}
+      {pokemonIndex < pokemonList.length - 1 && (
+        <button onClick={hanhdleClickIncr}>Suivant</button>
+      )}
     </div>
   );
 };
 
 NavBar.propTypes = {
+  setPokemonIndex: PropTypes.func.isRequired,
   pokemonIndex: PropTypes.number.isRequired,
-  pokemonList: PropTypes.array.isRequired,
-  hanhdleClickIncr: PropTypes.func.isRequired,
-  hanhdleClickDecr: PropTypes.func.isRequired
-}
+  pokemonList: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      imgSrc: PropTypes.string,
+    })
+  ).isRequired,
+};
 
 export default NavBar;
